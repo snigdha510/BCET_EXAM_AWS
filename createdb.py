430e56773dc8c54c40911c493843de26fa8e5af9
Manas Hejmadi <manas2all@outlook.com>
2024-05-02 23:25:39 +0530

    Fixed createdb.py

--- createdb.py
+++ createdb.py
@@ -1,4 +1,7 @@
 from pariksha import create_app,db
-with app.app_context():
-    print('DB Created')
-    db.create_all()
+
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        print('DB Created')
+        db.create_all()
